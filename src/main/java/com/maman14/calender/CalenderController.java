763c5baffd312661fc5db3b5a60808cd7fcf88c0
morsimha <mor.simha@intel.com>
2022-12-31 14:48:01 +0200

    organized

--- src/main/java/com/maman14/calender/CalenderController.java
+++ src/main/java/com/maman14/calender/CalenderController.java
@@ -35,17 +35,16 @@ public class CalenderController {
         DayOfWeek firstDayOfWeek = firstDayOfMonth.getDayOfWeek();
         int firstColumnIndex = firstDayOfWeek.getValue() % WIDTH;
         int currentDay = 1;
-
         for (int row = 0; row < HEIGHT; row++) {
             for (int column = 0; column < WIDTH; column++) {
-                if (row == 0 && column < firstColumnIndex) {
+                if ((row == 0 && column < firstColumnIndex) || currentDay > daysInMonth) {
                     buttons[row][column] = new Button("");
                     buttons[row][column].setDisable(true);
-                } else if (currentDay > daysInMonth) {
-                    buttons[row][column] = new Button("");
-                    buttons[row][column].setDisable(true);
+                    buttons[row][column].setPrefSize(daysGrade.getPrefWidth()/7,daysGrade.getPrefWidth()/7);
+
                 } else {
                     buttons[row][column] = new Button(String.valueOf(currentDay));
+                    buttons[row][column].setPrefSize(daysGrade.getPrefWidth()/7,daysGrade.getPrefWidth()/7);
                     currentDay++;
                 }
                 daysGrade.add(buttons[row][column], column, row);
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,8 @@
 module com.maman14.calender {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
+
+
     opens com.maman14.calender to javafx.fxml;
     exports com.maman14.calender;
 }
